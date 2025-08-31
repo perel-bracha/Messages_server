dd52cb34029e95747bed4e8c4a440aba10636516
perel-bracha <148152362+perel-bracha@users.noreply.github.com>
2025-08-31 22:02:58 +0300

    Update majors.services.js

--- src/Services/majors.services.js
+++ src/Services/majors.services.js
@@ -2,7 +2,7 @@ const pool = require("../DB/db");
 
 async function getAllMajors() {
   try {
-    const result = await pool.query("SELECT * FROM majors");
+    const result = await pool.query("SELECT * FROM majors WHERE major_id != ?", [19]);
     return result[0];
   } catch (err) {
     throw err;
